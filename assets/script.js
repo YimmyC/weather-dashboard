d48f4710e6872e9aa2212b1f48c79a47a49fac58
YimmyC <jim.cassano1@gmail.com>
2023-03-02 14:56:47 -0600

    started event listeners

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,25 @@
 // Global variables
 // divs on other side of HTML
+let btnSearch = document.querySelector("#btnSearch");
 
 // Functions
+function init() {
+  // grab last search result from local storage and display it
+}
+
+function search() {
+  alert("search button clicked");
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {});
+}
 
 // function calls and event listeners
+init();
+
+// search button event listener
+btnSearch.addEventListener("click", search);
+
+// past search button event listener
