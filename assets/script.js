8ab75bb47859df3142cab7db2986e177f7ba41ba
YimmyC <jim.cassano1@gmail.com>
2023-03-03 00:14:58 -0600

    card started

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 // Global variables
 // divs on other side of HTML
 let btnSearch = document.querySelector("#btnSearch");
+let currentCity = document.querySelector("#currentCity");
+let currentTemp = document.querySelector("#currentTemp");
+let currentWind = document.querySelector("#currentWind");
+let currentHumidity = document.querySelector("#currentHumidity");
 
 // Functions
 function init() {
@@ -9,7 +13,7 @@ function init() {
 
 function search() {
   // get city name from input
-  let cityName = "Chicago";
+  let cityName = `chicago`;
   console.log(cityName);
   let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=6e9967825cf147499a46407cb45cce16`;
 
@@ -21,6 +25,10 @@ function search() {
       console.log(data);
       // chicago
       // temp wind & humidity
+      currentCity.innerHTML = `${data.city.name}`;
+      currentTemp.innerHTML = `temp: ${data.list[0].main.temp}`;
+      currentWind.innerHTML = `wind: ${data.list[0].wind.speed}`;
+      currentHumidity.innerHTML = `Humidity: ${data.list[0].main.humidity}`;
       // 5 day forecast
     });
 }
