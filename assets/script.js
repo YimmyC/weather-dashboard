4c311a5583d8c98261f2d92b93dcb95c6866e0be
YimmyC <jim.cassano1@gmail.com>
2023-03-02 23:56:52 -0600

    got api key working

--- assets/script.js
+++ assets/script.js
@@ -8,12 +8,21 @@ function init() {
 }
 
 function search() {
-  alert("search button clicked");
+  // get city name from input
+  let cityName = "Chicago";
+  console.log(cityName);
+  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=6e9967825cf147499a46407cb45cce16`;
+
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {});
+    .then(function (data) {
+      console.log(data);
+      // chicago
+      // temp wind & humidity
+      // 5 day forecast
+    });
 }
 
 // function calls and event listeners
