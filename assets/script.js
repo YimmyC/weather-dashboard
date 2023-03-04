dde52035481ff82b6aa53bab15ea8c4d72a951fc
YimmyC <jim.cassano1@gmail.com>
2023-03-04 11:16:15 -0600

    started 5 day

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 // Global variables
 // divs on other side of HTML
 let btnSearch = document.querySelector("#btnSearch");
+let inputSearch = document.querySelector("#citySearch");
 let currentCity = document.querySelector("#currentCity");
 let currentTemp = document.querySelector("#currentTemp");
 let currentWind = document.querySelector("#currentWind");
 let currentHumidity = document.querySelector("#currentHumidity");
+let forecastCards = document.querySelector("#forecastCards");
 
 // Functions
 function init() {
@@ -13,9 +15,9 @@ function init() {
 
 function search() {
   // get city name from input
-  let cityName = `chicago`;
+  let cityName = inputSearch.value.trim();
   console.log(cityName);
-  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=6e9967825cf147499a46407cb45cce16`;
+  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=6e9967825cf147499a46407cb45cce16&units=imperial`;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -30,6 +32,18 @@ function search() {
       currentWind.innerHTML = `wind: ${data.list[0].wind.speed}`;
       currentHumidity.innerHTML = `Humidity: ${data.list[0].main.humidity}`;
       // 5 day forecast
+      data.list.forEach((day) => {
+        let midnight = day.dt_txt.split(" ")[1];
+        if (midnight === "00:00:00") {
+          console.log(day);
+          let dayCard = document.createElement("div");
+          dayCard.innerHTML += `<div>${day.dt_txt}</div>`;
+          dayCard.innerHTML += `<div>${day.main.temp}</div>`;
+          dayCard.innerHTML += `<div>${day.wind.speed}</div>`;
+          dayCard.innerHTML += `<div>${day.main.humidity}</div>`;
+          forecastCards.append(dayCard);
+        }
+      });
     });
 }
 
